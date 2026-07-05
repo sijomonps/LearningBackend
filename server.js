66a14031e8936b425ecf07de4ee3b54ef9cbb62a
sijomonps <sijomon700@gmail.com>
2026-07-05 19:31:09 +0530

    remved a unwanted log message from server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import path from 'path';
 import pageRoutes from "./routes/pageRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Server is running");
+});
 
 
 /*Middleware */
